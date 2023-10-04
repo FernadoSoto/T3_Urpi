38f5aef9b16a25ee1f68d076ddfab58e62b0509e
FernadoSoto <fernando.soto@vallegrande.edu.pe>
2023-10-04 12:41:18 -0500

    cambios

--- index.py
+++ index.py
@@ -1,7 +1,10 @@
-from flask import Flask, render_template
+#Flask es la clase principal que usamos para crear 
+from flask import Flask, render_template 
 
+#En esta línea, creamos una instancia de la aplicación Flask. 
 app = Flask(__name__)
 
+#Esta es una decoración de función que establece una ruta para la función 
 @app.route('/')
 def home():
     return render_template('index.html')
@@ -10,5 +13,6 @@ def home():
 def conga():
     return render_template('conga.html')
 
+#Esta línea verifica si el script se está ejecutando directamente
 if __name__ == '__main__':
     app.run()
